6ccbf61addd036caa5586a2d71f314d9e95417d9
Valerii Syroiezhin <v.syroiezhin@Air-Valerii.lan>
2022-08-02 02:09:20 +0300

    Update howdy.py

--- howdy.py
+++ howdy.py
@@ -1,1 +1,13 @@
-print("HOWDY")
+from email_to import Message, EmailServer
+
+def shape():
+  try: myhtml = f"""cool"""
+  except: myhtml = "ERROR :("
+  finally:
+    sender = "7kipgod@gmail.com"
+    destination = "svaller22@gmail.com"
+    
+    # create a password for the app bot and get the password by putting it in brackets : https://myaccount.google.com/apppasswords
+    EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(Message(myhtml), destination, "Попался, который кусался!")
+
+if __name__ == "__main__": shape()
