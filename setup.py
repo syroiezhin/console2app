09de97bfdc9ea0bced92255bf9385976296187c1
Valerii Syroiezhin <v.syroiezhin@MacBook-Air-Valerii.local>
2022-08-01 20:12:18 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+APP = ['agent.py']
+DATA_FILES = []
+OPTIONS = { 
+    'iconfile':'image.jpeg',
+    'argv_emulation': True,
+    'packages': ['PIL','os']
+ }
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+    install_requires=['Pillow','os'],
+)
