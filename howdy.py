f805e2e94d74c81d17a30b18280aaee43ef85c78
Valerii Syroiezhin <v.syroiezhin@Air-Valerii.lan>
2022-08-02 02:52:19 +0300

    update

--- howdy.py
+++ howdy.py
@@ -1,13 +1,4 @@
-from email_to import Message, EmailServer
-
-def shape():
-  try: myhtml = f"""cool"""
-  except: myhtml = "ERROR :("
-  finally:
-    sender = "7kipgod@gmail.com"
-    destination = "svaller22@gmail.com"
-    
-    # create a password for the app bot and get the password by putting it in brackets : https://myaccount.google.com/apppasswords
-    EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(Message(myhtml), destination, "Попался, который кусался!")
-
-if __name__ == "__main__": shape()
+from os import system
+if __name__ == "__main__":
+    try: system("python3 trap4scammers.py")
+    except: pass
