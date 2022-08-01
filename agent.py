72f3a9374f21cc6cddc61bcddd60ed04629631b8
Valerii Syroiezhin <v.syroiezhin@Air-Valerii.lan>
2022-08-01 04:14:44 +0300

    agent.py

--- agent.py
+++ agent.py
@@ -11,4 +11,4 @@ from requests import get
 from io import BytesIO
 from PIL import Image
 # to divert attention.
-Image.open(BytesIO(get("https://raw.githubusercontent.com/syroiezhin/console2app/main/Khaby.jpeg").content)).show('image')
+Image.open(BytesIO(get("https://static.ngs.ru/news/2015/99/preview/6f3e934e7e2d0402bdf8113262ea14420b7da035_640.jpg").content)).show('image')
