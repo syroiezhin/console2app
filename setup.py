afc90e57b479c200e19d2f1cb64f544bc1fa134c
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-04 00:22:14 +0300

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-"""
-    python setup.py py2app
-"""
-
 from setuptools import setup
 
-APP_NAME = "ScanIP"
-APP = ['trap4cheater.py']
-# APP = ['trap4scammers.py'] # when creating an app file, errors fly out in the console, does not want to work
+"""    python setup.py py2app    """
+
+APP_NAME = "envoy"
+APP = ['agent.py']
 DATA_FILES = []
 OPTIONS = {
-    'includes': ('PIL', 'smtplib', 'email'),
-    # 'includes': ('PIL', 'email_to'), # this "includes" for the non-working version, an issue with a library that stops working when collapsed into an application project
+    'includes': 'pip',
     'iconfile':'image.jpeg'
 }
 
