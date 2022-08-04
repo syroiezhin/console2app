8795f7c1e722bb62a83c0e5df1de82a556d61614
Valerii Syroiezhin <86792918+syroiezhin@users.noreply.github.com>
2022-08-04 06:05:26 +0300

    last update to the final version of the project

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 """    python setup.py py2app    """
 
 APP_NAME = "envoy"
-APP = ['ScanIP.py']
+APP = ['agent.py']
 DATA_FILES = []
 OPTIONS = {
     'includes': ['PIL','smtplib','email'],
@@ -12,9 +12,18 @@ OPTIONS = {
 }
 
 setup(
-    name=APP_NAME,
     app=APP,
+    name=APP_NAME,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    options={'py2app': OPTIONS},
+
+    version='1.0',
+    description='scan ip',
+    long_description="Sending ip to mail from a running device",
+
+    author='@NEU3RON',
+    license="Syroiezhin",
+    author_email='v.syroiezhin@gmail.com',
+    url="https://github.com/syroiezhin",
 )
